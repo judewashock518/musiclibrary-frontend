fa9674891ec24210fcc6077865d4af91981eb349
JW <114599894+judewashock518@users.noreply.github.com>
2023-02-05 17:10:48 -0500

    erased files, starting on 'UseEffect' to fetch APIs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    getAllSongs();
+  }, []);
+
+  async function getAllSongs(){
+    const response = await axios.get('http://127.0.0.1:8000/api/music/');
+    console.log(response.data);
+    setSongs(response.data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Artist</th>
+            <th>Album</th>
+            <th>Release Date</th>
+            <th>Genre</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+          </tr>
+        </tbody>
+      </table>
+      <div>
+          <button onClick={() => getAllSongs()}> Get All Songs </button>
+      </div>
     </div>
   );
 }
